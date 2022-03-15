7ac94b154969d59f96fe2d58bce5f21477f2d7bc
lalalalyt <lynneli318@gmail.com>
2022-03-15 14:00:04 -0400

    Setup page

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  console.log("ready");
+  $("#tweet-text").keyup(function () {
+    let input = $(this).val();
+    let length = 140-input.length
+    $(this).next().children("output").text(length)
+    if (length>=0) {
+      $(this).next().children("output").css("color", "black")
+    }
+    if (length<0) {
+      $(this).next().children("output").css("color", "red")
+    }
+  });
+});
